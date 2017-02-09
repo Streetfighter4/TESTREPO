12cd8a3fbda238130d05ef0423299f530886d1e7
Streetfighter4 <yassen4o222@abv.bg>
2017-02-09 14:24:11 +0200

    test 2

---
+++ sentrytest/home.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response
+app = Flask(__name__)
+
+from raven.contrib.flask import Sentry
+sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445ea94c1210d1c5e91d7@devmeter/2')
+
+
+@app.route('/')
+def hello_world():
+    try:
+        paca = paca4*2
+    except NameError:
+        sentry.captureException()
+    return Response(status=500)
+
+if __name__ == "__main__":
+    app.run(port=5000, host='192.168.0.100')
