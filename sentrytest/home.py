f9d05ec83414eb92088a6a6ac04af143a39b746e
Streetfighter4 <yassen4o222@abv.bg>
2017-02-10 12:50:51 +0200

    test 3

--- sentrytest/home.py
+++ sentrytest/home.py
@@ -5,10 +5,13 @@ from raven.contrib.flask import Sentry
 sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445ea94c1210d1c5e91d7@devmeter/2')
 
 
+
+
+
 @app.route('/')
 def hello_world():
     try:
-        paca = paca4*2
+        paca = paca14*2
     except NameError:
         sentry.captureException()
     return Response(status=500)
