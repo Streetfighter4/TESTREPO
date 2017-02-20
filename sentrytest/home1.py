70e2e6362db81c3dd6bcdbf8430299cee0525ebc
Streetfighter4 <yassen4o222@abv.bg>
2017-02-20 14:04:23 +0200

    make error1

--- sentrytest/home1.py
+++ sentrytest/home1.py
@@ -2,7 +2,7 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 from raven.contrib.flask import Sentry
-sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445ea94c1210d1c5e91d7@devmeter/2')
+sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445ea94c1210d1c5e91d7@devmeter:9000/2')
 
 
 
@@ -10,11 +10,12 @@ sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445
 
 @app.route('/')
 def hello_world():
+    print('hey')
     try:
-        paca = paca14*2
+        paca = paca77*2
     except NameError:
         sentry.captureException()
-    return Response(status=500)
+    return Response(status=200)
 
 if __name__ == "__main__":
     app.run(port=5000, host='192.168.0.100')
