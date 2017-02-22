8ec26e67ac7af39c791cd353dc463103b4495f83
Streetfighter4 <yassen4o222@abv.bg>
2017-02-22 15:35:30 +0200

    kaajsjas

--- sentrytest/home1.py
+++ sentrytest/home1.py
@@ -10,15 +10,14 @@ sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445
 
 
 
-
 @app.route('/')
 def hello_world():
     print('hey')
     try:
-        paca = paca103*2
+        paca = paca104*2
     except NameError:
         sentry.captureException()
     return Response(status=200)
 
 if __name__ == "__main__":
-    app.run(port=5000, host='192.168.0.100')
+    app.run(port=5000, host='127.0.0.1')
