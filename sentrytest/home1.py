b93f9a080d83cbfec71fefc2ef70e5035a71d582
Streetfighter4 <yassen4o222@abv.bg>
2017-02-22 15:57:55 +0200

    make error13

--- sentrytest/home1.py
+++ sentrytest/home1.py
@@ -5,20 +5,11 @@ from raven.contrib.flask import Sentry
 sentry = Sentry(app, dsn='http://7fe4ca23adb74d4d92f5d17167e66919:8fce1ff465b445ea94c1210d1c5e91d7@devmeter:9000/2')
 
 
-
-
-
-
-
-
-
-
-
 @app.route('/')
 def hello_world():
     print('hey')
     try:
-        paca = paca106*2
+        paca = paca107*2
     except NameError:
         sentry.captureException()
     return Response(status=200)
